9d12ed1e440059af1f539483dfccba9054f6b95b
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-06-01 03:16:02 -0400

    Aplicando navegacion

--- app/src/main/java/com/example/registroprestamos/MainActivity.kt
+++ app/src/main/java/com/example/registroprestamos/MainActivity.kt
@@ -1,43 +1,323 @@
 package com.example.registroprestamos
 
 import android.os.Bundle
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.registroprestamos.ui.theme.RegistroPrestamosTheme
+
+var selectedOcupacion: String? = null
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RegistroPrestamosTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MyApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RegistroPrestamosTheme {
-        Greeting("Android")
+        val navHostController = rememberNavController()
+
+        NavHost(navController = navHostController, startDestination = "ConsultaPersonas"){
+
+            composable("ConsultaPersonas"){
+                consultaPersonasScreen(navHostController = navHostController)
+            }
+            composable("ConsultaOcupaciones"){
+                consultaOcupacionesScreen(navHostController = navHostController)
+            }
+            composable("RegistroPersonas"){
+                RegistroPersonasScreen(navHostController = navHostController)
+            }
+            composable("RegistroOcupaciones"){
+                RegistroOcupacionesScreen(navHostController = navHostController)
+            }
+
+        }
     }
 }
+
+@Composable
+fun RowItems(nombre: String) {
+    Row() {
+        Text(nombre)
+    }
+}
+
+@Composable
+fun consultaPersonasScreen(navHostController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {Text("Consulta De Personas")}
+            )
+        }
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Button(onClick = { navHostController.navigate("ConsultaOcupaciones") }) {
+                Text(text = "Ocupaciones")
+            }
+
+            val listaNombres = listOf<String>("Maria", "Jose", "Pedro", "Juan")
+
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                items(listaNombres) { nombre ->
+                    RowItems(nombre = nombre)
+                }
+            }
+            Scaffold(
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = { navHostController.navigate("RegistroPersonas") }
+                    ) {
+                        Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                    }
+                },
+                floatingActionButtonPosition = FabPosition.End
+            ) {
+
+            }
+
+        }
+    }
+
+}
+
+@Composable
+fun RegistroPersonasScreen(navHostController: NavHostController) {
+
+    val listaOcupaciones =
+        listOf<String>("Profesor", "Ingeniero", "Doctor", "Herrero", "Carpintero")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {Text("Registro De Personas")}
+            )
+        }
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = { },
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Text(text = "Nombres")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Person, contentDescription = null)
+                }
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = { },
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Text(text = "Email")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Email, contentDescription = null)
+                }
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            OcupacionesSpinner(ocupacion = listaOcupaciones)
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = { },
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Text(text = "Salario")
+                }
+            )
+
+            Spacer(modifier = Modifier.height(100.dp))
+
+            Button(
+                onClick = { navHostController.navigateUp() },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "Guarduar")
+            }
+        }
+    }
+
+}
+
+@Composable
+fun OcupacionesSpinner(ocupacion: List<String>) {
+
+    var ocupacionText by remember {
+        mutableStateOf("")
+    }
+    var expended by remember {
+        mutableStateOf(false)
+    }
+    Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+        Row(Modifier
+            .fillMaxWidth()
+            .clickable {
+                expended = !expended
+            }
+            .padding(8.dp)
+        ) {
+            Text(text = ocupacionText, fontSize = 18.sp, modifier = Modifier.padding(end = 8.dp))
+            Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "")
+            DropdownMenu(
+                expanded = expended,
+                onDismissRequest = { expended = false }
+            ) {
+                ocupacion.forEach { ocupacion ->
+                    DropdownMenuItem(onClick = {
+
+                        expended = false
+                        ocupacionText = ocupacion.toString()
+                        selectedOcupacion = ocupacion
+                    }) {
+                        Text(text = ocupacion.toString())
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun RegistroOcupacionesScreen(navHostController: NavHostController) {
+
+    var person by rememberSaveable() {
+        mutableStateOf("")
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {Text("Registro De Ocupaciones")}
+            )
+        }
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            OutlinedTextField(
+                value = person,
+                onValueChange = { person = it },
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Text(text = "Nombre")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Build, contentDescription = null)
+                }
+            )
+
+            Spacer(modifier = Modifier.height(200.dp))
+
+            Button(
+                onClick = { navHostController.navigateUp() },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "Guarduar")
+            }
+        }
+    }
+}
+
+@Composable
+fun consultaOcupacionesScreen(navHostController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {Text("Consulta De Ocupaciones")}
+            )
+        }
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Button(onClick = { navHostController.navigate("ConsultaPersonas") }) {
+                Text(text = "Personas")
+            }
+
+            val listaOcupaciones =
+                listOf<String>("Profesor", "Ingeniero", "Doctor", "Herrero", "Carpintero")
+
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                items(listaOcupaciones) { nombre ->
+                    RowItems(nombre = nombre)
+                }
+            }
+            Scaffold(
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = { navHostController.navigate("RegistroOcupaciones") }
+                    ) {
+                        Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                    }
+                },
+                floatingActionButtonPosition = FabPosition.End
+            ) {
+
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
